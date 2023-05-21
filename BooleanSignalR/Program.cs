8204b09ef846b20e24d09ad62c992a7f80c0b6ba
DvD <dvd1202@gmail.com>
2023-05-21 22:44:35 +0200

    - SignalR client example

---
+++ BooleanSignalR/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Text;
+
+namespace BooleanSignalR
+{
+    internal class Program
+    {
+        public static string UserName { get; set; }
+        private static StringBuilder Buffer = new StringBuilder();
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Immetti il tuo username per entrare nella chat:");
+            UserName = Console.ReadLine();
+            await SignalRClient.Connect();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Enter:
+                            Console.WriteLine();
+                            await SignalRClient.Connection.InvokeAsync("SendMessageToAll", UserName, Buffer.ToString());
+                            Buffer.Clear();
+                            break;
+                        default:
+                            Buffer.Append(key.KeyChar);
+                            Console.Write(key.KeyChar);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BooleanSignalR/SignalRClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanSignalR
+{
+    public class SignalRClient
+    {
+        public const string URL = $"https://localhost:7228/ChatHub";
+        public static HubConnection Connection { get; set; }
+
+        public static async Task Connect()
+        {
+            try
+            {
+                Console.WriteLine("Connessione in corso...");
+                if (Connection != null)
+                    return;
+                Connection = new HubConnectionBuilder()
+                    .WithUrl(URL)
+                    .WithAutomaticReconnect()
+                    .Build();
+
+                Connection.On<string, string>("ReceiveMessageFromAll",
+                    (user, message) =>
+                    {
+                        Console.WriteLine($"{DateTime.Now:yyyy/MM/dd HH:mm:ss} {user}> {message}");
+                    });
+
+                await Connection.StartAsync();
+                Console.WriteLine("Connesso!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERRORE! {e}");
+            }
+        }
+    }
+}
